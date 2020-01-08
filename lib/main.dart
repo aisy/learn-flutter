024d76abbf0ebee86c15113b84f6f875c4666455
aisy <aisymuhammad@gmail.com>
2020-01-08 14:00:20 +0700

    using state in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,53 +3,63 @@ import 'package:flutter/material.dart';
 // Main function yang menjalankan class MyApp
 void main() => runApp(MyApp());
 
-// class MyApp membuat Widget dengan kondisi tanpa state
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int value = 0;
+
+  void tambahBil() {
+    setState(() {
+      value = value + 1;
+    });
+  }
+
+  void resetBil() {
+    setState(() {
+      value = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Membuat Widget bubble
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ),
-              Text(
-                'kandersteg, Switzerland',
-                style: TextStyle(color: Colors.grey[500]),
-              ),
-            ]),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
-
-    Widget detailSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        "Do consequat cillum do est excepteur reprehenderit adipisicing commodo culpa consequat. Occaecat sit magna cupidatat commodo aute elit aliquip. Occaecat adipisicing elit non dolor excepteur Lorem excepteur commodo sint. Mollit occaecat culpa enim culpa aute. Laboris qui exercitation ad ipsum minim ad anim.",
-        softWrap: true,
-      ),
-    );
-    //
     return MaterialApp(
-        // membuat halaman dengan isi appBar dan body
-        home: Scaffold(
-            body: ListView(
-      children: [titleSection, detailSection],
-    )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Yeaaaaah ALiii"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                value.toString(),
+                style: TextStyle(fontSize: 80),
+              ),
+              SizedBox(height: 10),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Text("tambah bilangan"),
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(15),
+                onPressed: tambahBil,
+              ),
+              SizedBox(height: 10),
+              RaisedButton(
+                child: Text("reset"),
+                color: Colors.red,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(15),
+                onPressed: resetBil,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
