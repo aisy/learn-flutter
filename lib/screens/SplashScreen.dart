63ccb70f802f4a035d7dc07d8dcf5b694a0a599c
aisy <aisymuhammad@gmail.com>
2020-01-10 11:24:07 +0700

    add splash screen

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:eleven_guitar/screens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return HomeScreen();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("data"),
+      ),
+    );
+  }
+}
