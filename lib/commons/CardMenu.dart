7be4ff1f1f85642843b7cacdad4d7bc56d4de096
aisy <aisymuhammad@gmail.com>
2020-01-10 16:56:55 +0700

    add border radius and change width

--- lib/commons/CardMenu.dart
+++ lib/commons/CardMenu.dart
@@ -2,28 +2,39 @@ import 'package:flutter/material.dart';
 
 class CardMenu extends StatelessWidget implements PreferredSizeWidget {
   final Text title;
-  final Icon icon;
+  final String icon;
 
-  CardMenu({this.icon, this.title});
+  CardMenu({
+    this.icon = "tv",
+    this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
-        width: 100,
-        height: 100,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.tv,
-                color: Colors.red,
-                size: 40.0,
-              ),
-              SizedBox(height: 5),
-              title,
-            ]),
+      color: Colors.blueGrey.shade700,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
       ),
+      child: Container(
+          width: 89,
+          height: 89,
+          child: GestureDetector(
+            onTap: () => AlertDialog(
+              title: Text("Test"),
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.tv,
+                    color: Colors.white,
+                    size: 40.0,
+                  ),
+                  SizedBox(height: 5),
+                  title,
+                ]),
+          )),
     );
   }
 
