d1a14d7f85e1c7565cc80616149cc626a080e07f
aisy <aisymuhammad@gmail.com>
2020-01-10 09:44:44 +0700

    add screen Home

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,51 @@
+import 'package:eleven_guitar/commons/AppBarMain.dart';
+import 'package:eleven_guitar/commons/CardMenu.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int value = 0;
+
+  void tambahBil() {
+    setState(() {
+      value = value + 1;
+    });
+  }
+
+  void resetBil() {
+    setState(() {
+      value = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarMain(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              value.toString(),
+              style: TextStyle(fontSize: 80),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: CardMenu(
+                title: Text(
+                  "TV",
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
